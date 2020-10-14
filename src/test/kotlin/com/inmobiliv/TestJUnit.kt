9043a168b7d7a95747357d210930c9428fe77666
rauldpm <rauldpm@correo.ugr.es>
2020-10-14 22:43:11 +0200

    Añadida clase inicial TestJUnit para testeo de la clase Vivienda, refs #12

---
+++ src/test/kotlin/com/inmobiliv/TestJUnit.kt
@@ -0,0 +1,13 @@
+
+package com.inmobiliv
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+
+class TestJUnit {
+
+
+
+}
