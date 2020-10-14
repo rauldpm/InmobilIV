a25595602153948927bc19d92980419735286ab8
rauldpm <rauldpm@correo.ugr.es>
2020-10-14 22:50:43 +0200

    Añadidos algunos test para la clase Vivienda, refs #12

--- src/test/kotlin/com/inmobiliv/TestJUnit.kt
+++ src/test/kotlin/com/inmobiliv/TestJUnit.kt
@@ -8,6 +8,28 @@ import org.junit.jupiter.api.BeforeEach
 
 class TestJUnit {
 
+    private lateinit var vivienda: Vivienda
 
+    @BeforeEach
+    fun conf() {
+        vivienda = Vivienda(5.2, 3, 16.8, "Calle Pepito", "Javier")
+    }
+
+    @Test
+    @DisplayName("Comprueba si el objeto se crea correctamente")
+    fun testClassMessage() {
+        val message = vivienda.getMessage()
+        assertThat("Soy una vivienda").isEqualTo(message)
+    }
+
+    @Test
+    @DisplayName("Comprueba si la clase se ha creado con los valores dados por el constructor")
+    fun testClassValues() {
+        assertThat(vivienda.superficie).isEqualTo(5.2)
+        assertThat(vivienda.habitaciones).isEqualTo(3)
+        assertThat(vivienda.precio).isEqualTo(16.8)
+        assertThat(vivienda.direccion).isEqualTo("Calle Pepito")
+        assertThat(vivienda.propietario).isEqualTo("Javier")
+    }
 
 }
