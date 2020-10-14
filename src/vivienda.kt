a10ded68461282034b01013465272de0c94b9631
rauldpm <rauldpm@correo.ugr.es>
2020-10-14 19:11:04 +0200

    Eliminada clase vivienda por reestructuracion de carpetas, refs #5

--- src/vivienda.kt
+++
@@ -1,8 +0,0 @@
-
-class Vivienda() {
-    val superficie: Double = 0.0
-    val habitaciones: Int = 0
-    val precio: Double = 0.0
-    val direccion: String = ""
-    val propietario: String = ""
-}
