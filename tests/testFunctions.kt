e75baefa00abd7063409c386b8ae131857c79315
rauldpm <rauldpm@correo.ugr.es>
2020-11-24 19:04:16 +0100

    Añadido test para testear la funcion serverless [skip travis][skip ci][skip jenkins], refs #29

---
+++ tests/testFunctions.kt
@@ -0,0 +1,18 @@
+package com.inmobiliv
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.assertj.core.api.Assertions.assertThat
+
+class TestFunctions {
+
+    @Test
+    @DisplayName("Comrpueba si el mensaje de la funcion serverless es correcto")
+    fun testClassInmueble() {
+        var mensaje = "InmobilIV Serverless Function v3"
+        var mensajeServerless = serverless()
+        // Llama al metodo "serverless"
+        assertThat(mensajeServerless).isEqualTo("InmobilIV Serverless Function v3")
+    }
+
+}
