a835f00194f41ef4e55b8f2a042bbde5a446514d
rauldpm <rauldpm@correo.ugr.es>
2020-11-26 02:38:32 +0100

    Añadido test para funcion serverless json [skip ci][skip jenkins], closes #29

--- tests/testFunctions.kt
+++ tests/testFunctions.kt
@@ -7,11 +7,48 @@ import org.assertj.core.api.Assertions.assertThat
 class TestFunctions {
 
     @Test
-    @DisplayName("Comprueba si el mensaje de la funcion serverless de aws es correcto")
+    @DisplayName("Comprueba si el mensaje de la funcion serverless / de aws es correcto")
     fun testClassFunAWS() {
         var mensaje = "InmobilIV Serverless Function v4"
         var mensajeServerless = serverless()
         assertThat(mensajeServerless).isEqualTo(mensaje)
+    }
+
+    @Test
+    @DisplayName("Comprueba si el mensaje de la funcion serverless /json de aws es correcto")
+    fun testClassFunJsonAWS() {
+        var dataI = """
+    {
+        "inmuebles" : [
+            {
+                "superficie": 140.0,
+                "habitaciones": 4,
+                "precio": 30056.15,
+                "vivienda": {
+                    "calle": "Maestro Faus",
+                    "portal": 4,
+                    "piso": 1,
+                    "letra": "D"
+                },
+                "propietario": "Josefo"
+            },
+            {
+                "superficie": 452.6,
+                "habitaciones": 5,
+                "precio": 50000.69,
+                "vivienda": {
+                    "calle": "Pontezuelas",
+                    "portal": 6,
+                    "piso": 5,
+                    "letra": "F"
+                },
+                "propietario": "Marijol"
+            }
+        ]   
+    }"""
+
+        var mensajeJson = json()
+        assertThat(mensajeJson).isEqualTo(dataI)
     }
 
 }
