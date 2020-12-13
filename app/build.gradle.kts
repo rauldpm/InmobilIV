2fba788dd3b372b4c725e3b13e11c89e59b5b966
rauldpm <rauldpm@correo.ugr.es>
2020-12-14 00:35:57 +0100

    Modificado sourceSets de ficheros [skip travis][skip ci][skip jenkins], refs #34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,30 +2,38 @@
 import io.kotless.plugin.gradle.dsl.kotless
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
+// Versiones 
+val assert_version = "3.12.2"
+val junit_version = "5.7.0"
+val ktor_version = "1.4.3"
+
+// Plugins a usar
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.20"
     id("io.kotless") version "0.1.6"
 }
 
-
+// Indica la estructura de archivos
 sourceSets {
     main {
         java {
-            setSrcDirs(listOf("main"))
+            setSrcDirs(listOf("app/src/main"))
         }
     }
     test {
         java {
-            setSrcDirs(listOf("tests"))
+            setSrcDirs(listOf("app/src/test"))
         }
     }
 }
 
+// Repositorios de donde obtiene las dependencias necesarias
 repositories {
     jcenter()
     mavenCentral()
 }
 
+// Dependencias del proyecto
 dependencies {
 
     // Align versions of all Kotlin components
@@ -44,19 +52,25 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
     // JUnit5 - Usado para los tests
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junit_version")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit_version")
 
     // Usado para las aserciones de los test
-    testImplementation("org.assertj:assertj-core:3.12.2")
+    testImplementation("org.assertj:assertj-core:$assert_version")
 
     // para kotless
-    implementation(kotlin("stdlib"))
     implementation("io.kotless", "kotless-lang", "0.1.6")
 
+    // framework ktor
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-freemarker:$ktor_version")
+
+    // loggin
+    compile("ch.qos.logback:logback-classic:1.2.3")
 }
 
+// Configuracion para serverless
 kotless {
    config { 
        bucket = "mi.kotlin.bucket"
@@ -75,12 +89,15 @@ kotless {
    }
    extensions {
         local {
-            useAWSEmulation = true
+            useAWSEmulation = false
         }
     }
-
 }
 
+// Indica la clase principal 
+application {
+    mainClassName = "com.inmobiliv.MainKt"
+}
 
 // Tarea que ejecuta el test principal
 tasks {
