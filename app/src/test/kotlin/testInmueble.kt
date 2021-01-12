df0b82dd86ea88772201f0f66b59385fbc795fc5
rauldpm <rauldpm@correo.ugr.es>
2021-01-12 12:19:17 +0100

    Eliminadas referencias a clase Vivienda eliminada, [skip travis][skip ci][skip jenkins], refs #37

--- app/src/test/kotlin/testInmueble.kt
+++ app/src/test/kotlin/testInmueble.kt
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.BeforeEach
 
 class TestInmueble {
 
-    private lateinit var vivienda: Vivienda
     private lateinit var inmueble: Inmueble
 
     @BeforeEach
     fun conf() {
-        vivienda = Vivienda("calle", 4, 5, 'A')
-        inmueble = Inmueble(5.2, 3, 16.8, vivienda, "Javier")
+        inmueble = Inmueble(5.2, 3, 16.8, "calle", 4, 5, 'A', "Javier")
     }
 
     @Test
@@ -23,7 +21,10 @@ class TestInmueble {
         assertThat(inmueble.getSup()).isEqualTo(5.2)
         assertThat(inmueble.getHab()).isEqualTo(3)
         assertThat(inmueble.getPre()).isEqualTo(16.8)
-        assertThat(inmueble.getViv()).isEqualTo(vivienda)
+        assertThat(inmueble.getCal()).isEqualTo("calle")
+        assertThat(inmueble.getPor()).isEqualTo(4)
+        assertThat(inmueble.getPis()).isEqualTo(5)
+        assertThat(inmueble.getLet()).isEqualTo('A')
         assertThat(inmueble.getPro()).isEqualTo("Javier")
     }
 
@@ -31,20 +32,24 @@ class TestInmueble {
     @DisplayName("Test Inmueble - Comprueba setters y getters")
     fun testSetGet() {
 
-        var viv = Vivienda("Alhondiga", 3, 4, 'D')
-
         inmueble.setSup(3.1)
         inmueble.setHab(4)
         inmueble.setPre(34.3)
-        inmueble.setViv(viv)
+        inmueble.setCal("Alhondiga")
+        inmueble.setPor(3)
+        inmueble.setPis(4)
+        inmueble.setLet('D')
         inmueble.setPro("Luis")
         inmueble.setID(3)
 
         assertThat(inmueble.getSup()).isEqualTo(3.1)
         assertThat(inmueble.getHab()).isEqualTo(4)
         assertThat(inmueble.getPre()).isEqualTo(34.3)
+        assertThat(inmueble.getCal()).isEqualTo("Alhondiga")
+        assertThat(inmueble.getPor()).isEqualTo(3)
+        assertThat(inmueble.getPis()).isEqualTo(4)
+        assertThat(inmueble.getLet()).isEqualTo('D')
         assertThat(inmueble.getPro()).isEqualTo("Luis")
-        assertThat(inmueble.getViv()).isEqualTo(viv)
         assertThat(inmueble.getID()).isEqualTo(3)
     }
 }
