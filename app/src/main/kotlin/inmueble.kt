057274e51e505e47e4229d69b2f0be3d38a9877b
rauldpm <rauldpm@correo.ugr.es>
2021-01-12 12:17:13 +0100

    Eliminada clase Vivienda, [skip travis][skip jenkins][skip ci], refs #37

--- app/src/main/kotlin/inmueble.kt
+++ app/src/main/kotlin/inmueble.kt
@@ -1,59 +1,45 @@
 
 package com.inmobiliv
 
-// Esta clase simula a un struct
-class Vivienda {
-    var calle: String = ""
-    var portal: Int = 0
-    var piso: Int = 0
-    var letra: Char = ' '
-
-    constructor(calle: String, portal: Int, piso: Int, letra: Char) {
-        this.calle = calle
-        this.portal = portal
-        this.piso = piso
-        this.letra = letra
-    }
-
-    fun getCal() = this.calle
-    fun getPor() = this.portal
-    fun getPis() = this.piso
-    fun getLet() = this.letra
-
-    fun setCal(cal: String) { calle = cal }
-    fun setPor(por: Int) { portal = por }
-    fun setPis(pis: Int) { piso = pis }
-    fun setLet(let: Char) { letra = let }
-
-}
-
 class Inmueble {
     var superficie: Double = 0.0
     var habitaciones: Int  = 0
     var precio: Double= 0.0
-    var vivienda: Vivienda
+    var calle: String = ""
+    var portal: Int = 0
+    var piso: Int = 0
+    var letra: Char = ' '
     var propietario: String = ""
     var id: Int = 0
 
-    constructor(sup: Double, hab: Int, pre: Double, vivienda: Vivienda, pro: String) {
+    constructor(sup: Double, hab: Int, pre: Double, cal: String, por: Int, pis: Int, let: Char, pro: String) {
         this.superficie = sup
         this.habitaciones = hab
         this.precio = pre
-        this.vivienda = vivienda
+        this.calle = cal
+        this.portal = por
+        this.piso = pis
+        this.letra = let
         this.propietario = pro
     }
 
     fun getSup() = this.superficie
     fun getHab() = this.habitaciones
     fun getPre() = this.precio
-    fun getViv() = this.vivienda
+    fun getCal() = this.calle
+    fun getPor() = this.portal
+    fun getPis() = this.piso
+    fun getLet() = this.letra
     fun getPro() = this.propietario
     fun getID()  = this.id
 
     fun setSup(sup: Double) { superficie = sup }
     fun setHab(hab: Int) { habitaciones = hab }
     fun setPre(pre: Double) { precio = pre }
-    fun setViv(viv: Vivienda) { vivienda = viv }
+    fun setCal(cal: String) { calle = cal }
+    fun setPor(por: Int) { portal = por }
+    fun setPis(pis: Int) { piso = pis }
+    fun setLet(let: Char) { letra = let }
     fun setPro(pro: String) { propietario = pro }
     fun setID(i: Int) { id = i }
 
