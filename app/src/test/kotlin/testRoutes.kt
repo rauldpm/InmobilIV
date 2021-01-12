2d053c265848bb787bb660ef5173f114c5baa006
rauldpm <rauldpm@correo.ugr.es>
2021-01-12 12:20:30 +0100

    Eliminados comentarios innecesarios, [skip travis][skip ci][skip jenkins], refs #37

--- app/src/test/kotlin/testRoutes.kt
+++ app/src/test/kotlin/testRoutes.kt
@@ -26,8 +26,7 @@ class TestRoutes {
             assertEquals(response.content, "{ status: \"OK\" }")
         }
     } 
-
-    // curl -X "GET" http://0.0.0.0:8080/inmuebles
+    
     @Test
     @DisplayName("Test Requests HTTP - Comprueba GET")
     fun testRouteGet() = withTestApplication(Application::module){
@@ -39,7 +38,6 @@ class TestRoutes {
         }
     }
 
-    // curl --data '{"superficie":"112.2","habitaciones":"3","precio":"12556.36","calle":"Pedro Antonio","portal":"3","piso":"4","letra":"F","propietario":"Javier Iañez"}' -H "Content-Type: application/json" --request POST http://0.0.0.0:8080/inmuebles
     @Test
     @DisplayName("Test Request HTTP - Comprueba POST")
     fun testPost() = withTestApplication(Application::module){
@@ -55,7 +53,6 @@ class TestRoutes {
         }
     }
 
-    // curl --data '{"superficie":"140.0","habitaciones":"4","precio":"30056.15","vivienda":{"calle":"Maestro Faus","portal":4,"piso":1,"letra":"D"},"propietario":"Magdalena"}' -H "Content-Type: application/json" --request PUT http://localhost:8080/inmuebles/0
     @Test
     @DisplayName("Test Request HTTP - Comprueba PUT")
     fun testPut() = withTestApplication(Application::module){
@@ -71,7 +68,6 @@ class TestRoutes {
         }
     }
 
-    // curl -X "DELETE" http://0.0.0.0:8080/inmuebles/0
     @Test
     @DisplayName("Test Request HTTP - Comprueba DELETE")
     fun testDelete() = withTestApplication(Application::module){
